c9e71926e28ffa4a55aeb9e0c1ed150967f790c6
Furkan <96385211+violencedev@users.noreply.github.com>
2022-05-05 21:51:16 +0300

    Delete fixer.js

--- fixer.js
+++
@@ -1,8 +0,0 @@
-const banner = document.querySelector('.banner')
-console.log(banner)
-
-window.onload = fixBackground = () => {
-    if (navigator.appVersion.indexOf("Mac") != -1) {
-        banner.classList.add('mac')
-    }
-}
