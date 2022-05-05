dfcade938ffa53d647587aed505bc6a22da87078
Furkan <96385211+violencedev@users.noreply.github.com>
2022-05-05 20:29:55 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-$(window).on('load', function(){
-    $('.loader').fadeOut(1000);
-    $('div#container').fadeIn(1000);
-    $('div#container').css('display', 'flex')
-});
-
-// Some JQuery Code Above This Comment Line
