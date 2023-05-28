1e9ed074e62879d4b702a4aba28b3f1f428601e4
Baran Önen <baranonenn@gmail.com>
2023-05-28 18:20:30 +0200

    Add 2022 website to archive

---
+++ 2022/src/basvuru.js
@@ -0,0 +1,7 @@
+const toggleBtn2 = document.querySelector('.toggle-btn'),
+navBar2 = document.querySelector('.navbar');
+
+
+toggleBtn2.addEventListener('click', () => {
+    navBar2.classList.toggle('active');
+})
